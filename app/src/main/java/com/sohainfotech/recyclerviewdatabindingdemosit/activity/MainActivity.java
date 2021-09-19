44046e2cfa6a025a7a00bfc6b6d5d45e3479108a
abduluptu <abduluptu@gmail.com>
2021-09-19 12:51:29 +0530

    New

--- app/src/main/java/com/sohainfotech/recyclerviewdatabindingdemosit/activity/MainActivity.java
+++ app/src/main/java/com/sohainfotech/recyclerviewdatabindingdemosit/activity/MainActivity.java
@@ -41,9 +41,26 @@ public class MainActivity extends AppCompatActivity {
     private void populateData() {
         List<DataModel> dataModelList = new ArrayList<>();
 
-        dataModelList.add(new DataModel("Android Oreo", "8.1"));
-        dataModelList.add(new DataModel("Android Nougat", "7.0"));
-        dataModelList.add(new DataModel("Android Marshmallow", "6.0"));
+        dataModelList.add(new DataModel("No codename", "1.0"));
+        dataModelList.add(new DataModel("No codename", "1.1"));
+        dataModelList.add(new DataModel("Cupcake", "1.5"));
+        dataModelList.add(new DataModel("Donut", "1.6"));
+        dataModelList.add(new DataModel("Eclair", "2.0 - 2.1"));
+        dataModelList.add(new DataModel("Froyo", "2.2 - 2.2.3"));
+        dataModelList.add(new DataModel("Gingerbread", "2.3 - 2.3.7"));
+        dataModelList.add(new DataModel("Honeycomb", "3.0 - 3.2.6"));
+        dataModelList.add(new DataModel("Ice Cream Sandwich", "4.0 - 4.0.4"));
+        dataModelList.add(new DataModel("Jelly Bean", "4.1 - 4.3.1"));
+        dataModelList.add(new DataModel("KitKat", "4.4 - 4.4.4"));
+        dataModelList.add(new DataModel("Lollipop", "5.0 - 5.1.1"));
+        dataModelList.add(new DataModel("Marshmallow", "6.0 - 6.0.1"));
+        dataModelList.add(new DataModel("Nougat", "7.0 - 7.1.0 - 7.1.2"));
+        dataModelList.add(new DataModel("Oreo", "8.0 - 8.1"));
+
+        dataModelList.add(new DataModel("Pie", "9.0"));
+        dataModelList.add(new DataModel("Android 10", "10.0"));
+        dataModelList.add(new DataModel("Android 11", "11"));
+        dataModelList.add(new DataModel("Android 12", "12"));
 
         binding.recyclerView.setAdapter(new DataAdapter(dataModelList, this));
 
